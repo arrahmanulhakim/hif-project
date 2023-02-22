3fe8a31e62ac37a98753638be03035f1440c1dd2
arrahmanulhakim <hakimarrahmannul@gmail.com>
2023-02-22 20:25:50 +0700

    init push ke github

---
+++ bin/hif_project1.dart
@@ -0,0 +1,5 @@
+import 'package:hif_project1/hif_project1.dart' as hif_project1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hif_project1.calculate()}!');
+}
---
+++ test/hif_project1_test.dart
@@ -0,0 +1,8 @@
+import 'package:hif_project1/hif_project1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
